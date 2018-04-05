587a7f8c4e7a1ea1316f5df222bf1059513a0b9f
opet <opet@Academico.edu>
2018-04-04 22:12:05 -0300

    initial commit

---
+++ app/src/main/java/com/iservice/opet/iservice/MainActivity.java
@@ -0,0 +1,17 @@
+package com.iservice.opet.iservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.stetho.Stetho;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Stetho.initializeWithDefaults(this);
+
+    }
+}
